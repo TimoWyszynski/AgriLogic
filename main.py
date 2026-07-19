0b19c76243b6bfa25d64ab0d7ccc3d0f4d3ca54c
Timo Wyszynski <timo.wyszynski@gmail.com>
2026-07-19 07:25:02 +0200

    Refoactored the code to actually work event based. Each subprocess of a process chain should be defined as a separate event like driving to the fild, working on the field, return etc. Subprocesses are within the agent classes, the manager combines them into specific process chains.

--- main.py
+++ main.py
@@ -3,25 +3,18 @@ import simpy
 def main():
     env = simpy.Environment()
 
-    vehicle_tank = simpy.Container(env, capacity=100, init=100)
-    vehicle = Vehicle(env, 20, 5, vehicle_tank, 100, 300)
+    vehicle_tank = simpy.Container(env, capacity=350, init=350)
+    vehicle = Vehicle(env, 25, 5, vehicle_tank, 10, 15)
 
     field_harvest = simpy.Container(env, 10, 10)
     field = Field(10, 3, field_harvest)
-    yard = Yard(simpy.Container(env, capacity=1000, init=1000), 0)
 
-    env.process(run_simulation(env, vehicle, field, yard))
+    yard = Yard(simpy.Container(env, capacity=10000, init=10000), 0)
+
+    manager = Manager(env, yard, field, vehicle)
+
+    env.process(manager.simple_process())
     env.run()
-
-
-def run_simulation(env, vehicle, field, yard):
-    while True:
-        print(f"Vehicle fuel capacity {vehicle.tank.level}")
-
-        try:
-            yield env.process(vehicle.drive_from_yard_to_field(env, field, yard))
-        except simpy.Interrupt:
-            break
 
 
 class Vehicle:
@@ -35,32 +28,41 @@ class Vehicle:
             field_energy_demand
         ):
         self.env = env
-        self.driving_speed = driving_speed
-        self.area_performance = area_performance
-        self.tank = tank
-        self.road_energy_demand = road_energy_demand
-        self.field_energy_demand = field_energy_demand
+        self.driving_speed = driving_speed                  #km/h
+        self.area_performance = area_performance            #ha/h
+        self.tank = tank                                    #L
+        self.road_energy_demand = road_energy_demand        #L/h
+        self.field_energy_demand = field_energy_demand      #L/ha
 
 
-    def drive_from_yard_to_field(self, env, field, yard):
-        remaining_distance = abs(yard.coordinates - field.coordinates) * 1000
-        while True:
+    def drive_between_yard_and_field(self, env, field, yard):
+        distance = abs(yard.coordinates - field.coordinates)
+        time = distance / self.driving_speed
+        energy = time * self.road_energy_demand
 
-            energy_demand = (self.road_energy_demand) * 1/1000
+        if energy >= self.tank.level:
+            raise simpy.Interrupt("Insufficient fuel to reach field.")
+        
+        yield self.tank.get(energy)
+        yield env.timeout(time)
 
-            if energy_demand >= self.tank.level:
-                print(f"Vehciel fuel is empty")
-                raise simpy.Interrupt(None)
+        print(f"Reached the field in {time} hours using {energy} liters Diesel.")
+        return
+    
 
-            yield self.tank.get((self.road_energy_demand) * 1/1000)
-            yield env.timeout(1)
+    def work_on_field(self, env, field):
+        time = field.field_area / self.area_performance
+        energy = self.field_energy_demand * field.field_area
 
-            remaining_distance -= (self.driving_speed / 3.6)
-            print(remaining_distance)
+        if energy >= self.tank.level:
+            raise simpy.Interrupt("Insufficient fuel to finish fieldwork.")
+        
+        yield self.tank.get(energy)
+        yield env.timeout(time)
 
-            if remaining_distance <= 0:
-                print(f"Reached the field.")
-                break
+        print(f"Finished fieldwork in {time} hours using {energy} liters Diesel.")
+        return
+
 
 class Field:
     def __init__(self, field_area, coordinates, harvest):
@@ -74,9 +76,19 @@ class Yard:
         self.fuel_storage = fuel_storage
         self.coordinates = coordinates
 
+
 class Manager:
-    def __init__(self):
-        pass
+    def __init__(self, env, yard, field, vehicle):
+        self.env = env
+        self.yard = yard
+        self.field = field
+        self.vehicle = vehicle
+
+    def simple_process(self):
+        yield self.env.process(self.vehicle.drive_between_yard_and_field(self.env, self.field, self.yard))
+        yield self.env.process(self.vehicle.work_on_field(self.env, self.field))
+        yield self.env.process(self.vehicle.drive_between_yard_and_field(self.env, self.field, self.yard))
+
 
 if __name__ == "__main__":
     main()
